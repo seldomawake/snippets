9578f6d393a0cfc0c266aea965873bb4ab76e848
Hammad Khan <hammad.t.khan@gmail.com>
2017-04-28 16:08:08 -0700

    Implementing main functionality

--- JsonToCsvUngrouper.py
+++ JsonToCsvUngrouper.py
@@ -1,10 +1,13 @@
 import csv
 import json
 
+
 class JsonToCsvUngrouper:
 
     def __init__(self, csv_file, cols_to_explode):
         self.csv_file = csv_file
+        self.raw_rows = None
+        self.col_headers = None
 
         if not hasattr(cols_to_explode, '__iter__'):
             self.cols_to_explode = [cols_to_explode]
@@ -12,13 +15,52 @@ class JsonToCsvUngrouper:
             self.cols_to_explode = cols_to_explode
         self.col_nums_to_explode = []
 
-    def read_csv(self):
+    def get_exploded_rows(self):
         with open(self.csv_file, 'rb') as csvfile:
             lines = csv.reader(csvfile, delimiter=',', quotechar='"')
             first_row = lines.next()
+            for row in lines:
+                self.reset_col_headers()
+                ungrouped_rows = self.explode_rows(rows=[row], col_nums_to_explode_by=self.col_nums_to_explode)
+                toret = iter(ungrouped_rows)
 
-            # assume that the first row is column headers
-            for i in range(len(first_row)):
-                if first_row[i] in self.cols_to_explode:
-                    self.col_nums_to_explode.append(i)
+                #yield from toret
+                for t in toret:
+                    yield t
 
+    def reset_col_headers(self):
+        if self.col_headers is None:
+            with open(self.csv_file, 'rb') as csvfile:
+                lines = csv.reader(csvfile, delimiter=',', quotechar='"')
+                # assume that the first row is column headers
+                self.col_headers = lines.next()
+
+        for i in range(len(self.col_headers)):
+            if self.col_headers[i] in self.cols_to_explode:
+                self.col_nums_to_explode.append(i)
+
+    def explode_rows(self, rows, col_nums_to_explode_by):
+        assert isinstance(col_nums_to_explode_by, list)
+        assert isinstance(rows, list)
+
+        if len(col_nums_to_explode_by) == 0:
+            return rows
+
+        unwrapped_rows = []
+        col_to_explode = col_nums_to_explode_by.pop()
+
+        for row in rows:
+            a_row_without_col_to_explode = [v for k, v in enumerate(row) if k != col_to_explode]
+
+            # cols to ungroup by are assumed to be in json objects -- can improve this further
+            # (if the .loads throws, assume it is an array of strings with len 1.)
+            exploded_col_as_list = json.loads(row[col_to_explode])
+
+            for elem in exploded_col_as_list:
+                row_with_exploded_col = a_row_without_col_to_explode[:]
+                row_with_exploded_col.insert(col_to_explode, elem)
+                unwrapped_rows.append(row_with_exploded_col)
+
+        unwrap_more = self.explode_rows(unwrapped_rows, col_nums_to_explode_by)
+
+        return unwrap_more
--- tests/TestJsonToCsvUngrouper.py
+++ tests/TestJsonToCsvUngrouper.py
@@ -14,12 +14,72 @@ class TestJsonToCsvUngrouper(unittest.TestCase):
         j = JsonToCsvUngrouper('./test.csv', 'col_2')
         self.assertIsNotNone(j)
         try:
-            j.read_csv()
+            j.reset_col_headers()
             self.assertTrue(3 in j.col_nums_to_explode)
         except Exception as e:
             print e
             self.fail()
 
+    def test_file_explode(self):
+        j = JsonToCsvUngrouper('./test.csv', 'col_2')
+        self.assertIsNotNone(j)
+        exploded_rows = []
+        try:
+            for r in j.get_exploded_rows():
+                exploded_rows.append(r)
+
+            self.assertEqual(7, len(exploded_rows))
+            self.assertEquals(["1,000", "email@address.com", '4', 2], exploded_rows[0])
+            self.assertEquals(["1,000", "email@address.com", '4', 3], exploded_rows[1])
+            self.assertEquals(["1,000", "email@address.com", '4', 4], exploded_rows[2])
+            self.assertEquals(["1,000", "email@address.com", '4', 5], exploded_rows[3])
+            self.assertEquals(["1,001", "email@address.com", '5', 6], exploded_rows[4])
+            self.assertEquals(["1,001", "email@address.com", '5', 7], exploded_rows[5])
+            self.assertEquals(["1,002", "email@address.com", '1', 40], exploded_rows[6])
+
+        except Exception as e:
+            print e
+            self.fail()
+
+    def test_explode_row(self):
+        j = JsonToCsvUngrouper('./test.csv', 'col_2')
+
+        rows = [["1,000", "email@address.com", 4, "[2,3,4,5]"],
+                ["1,000", "email@address.com", 4, "[6,7,8]"]]
+        cols_to_explode = [3]
+
+        splody = j.explode_rows(rows=rows, col_nums_to_explode_by=cols_to_explode)
+
+        self.assertEquals(7, len(splody))
+        self.assertEquals(["1,000", "email@address.com", 4, 2], splody[0])
+        self.assertEquals(["1,000", "email@address.com", 4, 3], splody[1])
+        self.assertEquals(["1,000", "email@address.com", 4, 4], splody[2])
+        self.assertEquals(["1,000", "email@address.com", 4, 5], splody[3])
+        self.assertEquals(["1,000", "email@address.com", 4, 6], splody[4])
+        self.assertEquals(["1,000", "email@address.com", 4, 7], splody[5])
+        self.assertEquals(["1,000", "email@address.com", 4, 8], splody[6])
+
+    def test_explode_row_multiple_cols(self):
+        j = JsonToCsvUngrouper('./test.csv', 'col_2')
+
+        rows = [['["lorem", "ipsum"]', "email@address.com", 4, "[2,3,4,5]"],
+                ['["1,000"]', "email@address.com", 4, "[6,7,8]"]]
+        cols_to_explode = [0, 3]
+
+        splody = j.explode_rows(rows=rows, col_nums_to_explode_by=cols_to_explode)
+
+        self.assertEquals(11, len(splody))
+        self.assertEquals(["lorem", "email@address.com", 4, 2], splody[0])
+        self.assertEquals(["ipsum", "email@address.com", 4, 2], splody[1])
+        self.assertEquals(["lorem", "email@address.com", 4, 3], splody[2])
+        self.assertEquals(["ipsum", "email@address.com", 4, 3], splody[3])
+        self.assertEquals(["lorem", "email@address.com", 4, 4], splody[4])
+        self.assertEquals(["ipsum", "email@address.com", 4, 4], splody[5])
+        self.assertEquals(["lorem", "email@address.com", 4, 5], splody[6])
+        self.assertEquals(["ipsum", "email@address.com", 4, 5], splody[7])
+        self.assertEquals(["1,000", "email@address.com", 4, 6], splody[8])
+        self.assertEquals(["1,000", "email@address.com", 4, 7], splody[9])
+        self.assertEquals(["1,000", "email@address.com", 4, 8], splody[10])
 
 if __name__ == '__main__':
     unittest.main()
